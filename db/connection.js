8db5d4da9729ce3bdca679b6382cc466e6ff29ba
goodwinamundson <goodwinamundson@gmail.com>
2022-05-19 14:27:30 +0200

    add schema files

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+// Create connection
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+// Connect to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME
+});
+
+module.exports = connection;
---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+-- Start of department table
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+-- Start of role table
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER
+);
+
+-- Start of employee table
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
